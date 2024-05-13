948cd89d8e1b50ebb2576aef50d2791f7f32b90b
PetarFOI <162334861+PetarFOI@users.noreply.github.com>
2024-05-13 18:03:58 +0200

    Add files via upload

---
+++ Software/EkoInventar/EkoInventar.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace EkoInventar
+{
+    public partial class EkoInventar : Form
+    {
+        public EkoInventar()
+        {
+            InitializeComponent();
+        }
+
+
+        private void txtUser_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtUser_Enter(object sender, EventArgs e)
+        {
+            if (txtUser.Text == "Korisničko ime")
+            {
+                txtUser.Text = "";
+                txtUser.ForeColor = Color.Black;
+            }
+
+        }
+
+        private void txtUser_Leave(object sender, EventArgs e)
+        {
+
+            if (txtUser.Text == "")
+            {
+                txtUser.Text = "Korisničko ime";
+                txtUser.ForeColor = Color.Silver;
+            }
+        }
+
+        private void txtPass_Enter(object sender, EventArgs e)
+        {
+            if (txtPass.Text == "Lozinka")
+            {
+                txtPass.Text = "";
+                txtPass.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtPass_Leave(object sender, EventArgs e)
+        {
+            if (txtPass.Text == "")
+            {
+                txtPass.Text = "Lozinka";
+                txtPass.ForeColor = Color.Silver;
+            }
+        }
+
+        private void picLogo_Click(object sender, EventArgs e)
+        {
+
+            Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+        }
+
+        private void picLogo_MouseEnter(object sender, EventArgs e)
+        {
+            picLogo.Cursor = Cursors.Hand;
+        }
+
+
+        private void EkoInventar_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cbPass_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnPrijava_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
